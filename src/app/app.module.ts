77758fefd47a2dbf38c72d6060738c4496915fb5
momeen <moomensaleem135@gmail.com>
2022-03-04 21:20:19 +0500

    Todo App

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { TodoModule } from './todos/todo.module';
+import { AuthModule } from './user/user.module';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/todo-app-db'),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      debug: false,
+    }),
+    TodoModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
